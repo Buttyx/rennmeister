7fe357d837265ec2a3ddf4ccda10d59087c6b56e
Xavier Butty <buttyxavier@gmail.com>
2016-03-12 09:48:53 +0100

    Update styling

--- modules/trackinginfos/client/config/trackinginfos.client.config.js
+++ modules/trackinginfos/client/config/trackinginfos.client.config.js
@@ -9,23 +9,23 @@
 
   function menuConfig(Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', {
-      title: 'Trackinginfos',
-      state: 'trackinginfos',
-      type: 'dropdown',
-      roles: ['*']
-    });
+    // Menus.addMenuItem('topbar', {
+    //   title: 'Trackinginfos',
+    //   state: 'trackinginfos',
+    //   type: 'dropdown',
+    //   roles: ['*']
+    // });
 
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'trackinginfos', {
-      title: 'List Trackinginfos',
-      state: 'trackinginfos.list'
-    });
+    // // Add the dropdown list item
+    // Menus.addSubMenuItem('topbar', 'trackinginfos', {
+    //   title: 'List Trackinginfos',
+    //   state: 'trackinginfos.list'
+    // });
 
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'trackinginfos', {
-      title: 'Create Trackinginfo',
-      state: 'trackinginfos.create'
-    });
+    // // Add the dropdown create item
+    // Menus.addSubMenuItem('topbar', 'trackinginfos', {
+    //   title: 'Create Trackinginfo',
+    //   state: 'trackinginfos.create'
+    // });
   }
 })();
